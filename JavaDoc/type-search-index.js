39cebc1575f5d1ba4a69c1a8e7419b4d84b2f6f2
Konstantin <98838016+Qantrex@users.noreply.github.com>
2024-12-02 10:35:56 +0100

    JavaDocs generated

---
+++ JavaDoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"RectangleController"},{"p":"<Unnamed>","l":"RectangleModel"},{"p":"<Unnamed>","l":"RectangleView"}];updateSearchResults();
